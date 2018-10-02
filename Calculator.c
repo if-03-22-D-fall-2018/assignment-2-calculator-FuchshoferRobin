d03654eef825bbf737c2079be09c9295a32380b4
FuchshoferRobin <robinfuchshofer@gmail.com>
2018-10-02 11:21:23 -0700

    Finnished Calculator

--- Calculator.c
+++ Calculator.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <float.h>
 
 double add(double operand1, double operand2){
   double sum = operand1+operand2;
@@ -17,31 +18,9 @@ double division(double operand1, double operand2){
   return div;
 }
 
-
-int main(int argc, char const *argv[]) {
-  double firstOperand;
-  double secondOperand;
+double PerformOperation(int type_of_operation, double firstOperand, double secondOperand)
+{
   double solution;
-  int type_of_operation;
-
-  do {
-
-  printf("Choose one of the following operations\n");
-  printf("Add(1)\n");
-  printf("Subtract(2)\n");
-  printf("Multiply(3)\n");
-  printf("Devide(4)\n");
-  printf("Stop program(-1)\n");
-  scanf("%d", &type_of_operation);
-  }
-  while(type_of_operation != 1 && type_of_operation != -1 && type_of_operation != 2 &&type_of_operation != 3 &&type_of_operation != 4);
-
-  if(type_of_operation != -1)
-  {
-  printf("Please enter first operand: \n");
-  scanf("%lf", &firstOperand);
-  printf("Please enter second operand: \n");
-  scanf("%lf", &secondOperand);
 
   if (type_of_operation == 1) {
     solution = add(firstOperand, secondOperand);
@@ -56,8 +35,65 @@ int main(int argc, char const *argv[]) {
     solution = division(firstOperand, secondOperand);
   }
 
-  printf("Die Lösung ist: %4.2f \n", solution);
+  if (solution < DBL_MIN) {
+   printf("Number underflow\n");
+   return 0;
+  }
+  else if (solution > DBL_MAX) {
+    printf("Number overflow\n");
+    return 0;
   }
 
+return solution;
+}
+
+
+int GetOperands()
+{
+  int type_of_operation;
+
+  do {
+
+  printf("Choose one of the following operations\n");
+  printf("Add(1)\n");
+  printf("Subtract(2)\n");
+  printf("Multiply(3)\n");
+  printf("Devide(4)\n");
+  printf("Stop program(-1)\n");
+  scanf("%d", &type_of_operation);
+  }
+  while(type_of_operation != 1 && type_of_operation != -1 && type_of_operation != 2 &&type_of_operation != 3 &&type_of_operation != 4);
+
+  return type_of_operation;
+}
+
+
+int main(int argc, char const *argv[]) {
+
+  int type_of_operation;
+  double solution;
+  double firstOperand;
+  double secondOperand;
+
+  type_of_operation = GetOperands();
+  if (type_of_operation != -1) {
+
+  printf("Please enter first operand: \n");
+  scanf("%lf", &firstOperand);
+
+  printf("Please enter second operand: \n");
+  scanf("%lf", &secondOperand);
+  if(type_of_operation == 4 && secondOperand == 0)
+  {
+    printf("You can not devide by 0\n");
+    return 0;
+  }
+  solution = PerformOperation(type_of_operation, firstOperand, secondOperand);
+    if(solution != 0)
+    {
+      printf("Die Lösung ist: %4.2f \n", solution);
+    }
+
+}
   return 0;
 }
